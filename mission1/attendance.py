e52e18c34d84ed5111fba598758308bfeb158945
yoondayoung <agwin73@naver.com>
2025-08-29 12:52:27 +0900

    remove player id from player data for mission 1

--- mission1/attendance.py
+++ mission1/attendance.py
@@ -1,9 +1,9 @@
 import os
 
 
-def add_new_player(player_id, day) -> list:
-    # player_dict{'name' : [id, attendance[요일], point, grade]}
-    new_player_data = [player_id, [0] * 7, 0, 0]
+def add_new_player(day) -> list:
+    # player_dict{'name' : [attendance[요일], point, grade]}
+    new_player_data = [[0] * 7, 0, 0]
     return update_player(new_player_data, day)
 
 
@@ -40,8 +40,8 @@ def update_player(player_data, day) -> list:
     elif day == "sunday":
         add_point = 2
 
-    player_data[1][day_index] += 1
-    player_data[2] += add_point
+    player_data[0][day_index] += 1
+    player_data[1] += add_point
 
     return player_data
 
@@ -54,7 +54,7 @@ def get_attendance_data_from_file(file) -> dict:
             if len(attendance) == 2:
                 player_name = attendance[0]
                 if player_name not in player_dict:
-                    player_dict[player_name] = add_new_player(len(player_dict), attendance[1])
+                    player_dict[player_name] = add_new_player(attendance[1])
                 else:
                     player_dict[player_name] = update_player(player_dict[player_name], attendance[1])
 
@@ -72,10 +72,10 @@ def print_attendance_system(file):
         print("파일을 찾을 수 없습니다.")
 
     for name, data in player_dict.items():
-        player_dict[name][2] += calculate_player_bonus_points(player_dict[name])
-        player_dict[name][3] = get_player_grade(player_dict[name][2])
+        player_dict[name][1] += calculate_player_bonus_points(player_dict[name])
+        player_dict[name][2] = get_player_grade(player_dict[name][2])
 
-        print_grade(name, player_dict[name][2], player_dict[name][3])
+        print_grade(name, player_dict[name][1], player_dict[name][2])
 
     print_removed_player(player_dict)
 
@@ -91,7 +91,7 @@ def get_player_grade(player_point):
 
 def calculate_player_bonus_points(player_data):
     bonus_point = 0
-    if player_data[1][2] > 9:
+    if player_data[0][2] > 9:
         bonus_point += 10
     if get_player_weekend_attendance(player_data) > 9:
         bonus_point += 10
@@ -99,7 +99,7 @@ def calculate_player_bonus_points(player_data):
 
 
 def get_player_weekend_attendance(player_data):
-    return player_data[1][5] + player_data[1][6]
+    return player_data[0][5] + player_data[0][6]
 
 
 def print_removed_player(player_dict):
@@ -110,8 +110,8 @@ def print_removed_player(player_dict):
             print(player_name)
 
 
-def is_player_removed(data):
-    return data[3] not in (1, 2) and data[1][2] == 0 and get_player_weekend_attendance(data) == 0
+def is_player_removed(player_data):
+    return player_data[2] not in (1, 2) and player_data[0][2] == 0 and get_player_weekend_attendance(player_data) == 0
 
 
 def print_grade(player_name, player_point, player_grade):
